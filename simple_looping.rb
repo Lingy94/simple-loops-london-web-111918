30ebb3f2d7040f8c2321de5705f0ef20b7bd3167
Jack Ling <j.lingyx@gmail.com>
2018-10-09 20:29:22 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -16,8 +16,8 @@ def times_iterator(number_of_times)
    phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times.times do
   put phrase
-  
-  
+end
+
 end
 
 def while_iterator(number_of_times)
