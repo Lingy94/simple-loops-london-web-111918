8d012cf1a8d5288bc88354b65ef42d5b16708f54
Jack Ling <j.lingyx@gmail.com>
2018-10-09 20:14:51 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,7 +13,7 @@ def loop_iterator(number_of_times)
     phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase 
     counter += 1
-    break when counter == number_of_times
+    break if counter == number_of_times
 end
 end
 
