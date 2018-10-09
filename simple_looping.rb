338ef1c0ff7b11c9299c0501480be1c599b2e440
Jack Ling <j.lingyx@gmail.com>
2018-10-09 20:36:55 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -38,7 +38,7 @@ end
 end
 
 def for_iterator(number_of_times)
-  range = 0..number_of_times
+  range = 1..number_of_times
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
